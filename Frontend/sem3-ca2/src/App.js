b50ccc4f62fe9f8726353ad86252957a436b9956
AlexKenfelt <cph-ak428@cphbusiness.dk>
2021-11-10 11:39:20 +0100

    visual

---
+++ Frontend/sem3-ca2/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState,useEffect } from "react"
+import facade from "./apiFacade";
+import LoggedIn from "./Loggedin";
+import LogIn from "./Login";
+ 
+
+function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+ 
+  const logout = () => { facade.logout()
+    setLoggedIn(false) } 
+  const login = (user, pass) => {facade.login(user,pass)
+    .then(res =>setLoggedIn(true));} 
+ 
+  return (
+    <div>
+      {!loggedIn ? (<LogIn login={login} />) :
+        (<div>
+          <LoggedIn facade={facade} />
+          <button onClick={logout}>Logout</button>
+        </div>)}
+    </div>
+  )
+ 
+}
+export default App;

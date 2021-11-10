3749a07cbb439920ca59f06e29392d9b701f7fe9
Julius Madsen <2330051117@protonmail.com>
2021-11-10 22:11:51 +0100

    security updates

--- src/main/java/security/SharedSecret.java
+++ src/main/java/security/SharedSecret.java
@@ -2,13 +2,20 @@ package security;
 
 import java.security.SecureRandom;
 
-/* This generates a secure random per execution of the server
+/*
+ * This generates a secure random per execution of the server
  * A server restart, will generate a new key, making all existing tokens invalid
- * For production (and if a load-balancer is used) come up with a persistent key strategy */
+ * For production (and if a load-balancer is used) come up with a persistent key strategy
+ */
+
+//TODO: Do this before deployment!
+
 public class SharedSecret {
     private static byte[] secret;
+
     public static byte[] getSharedKey() {
-      /*
+
+        /*
         System.out.println("******************* IMPORTANT ******************'");
         System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
@@ -17,8 +24,10 @@ public class SharedSecret {
         System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         */
+
         //REMOVE BEFORE PRODUCTION
-        if(true){
+
+        if (true) {
             return "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA".getBytes();
         }
         if (secret == null) {  //Or better read as an environment variable set on production server
